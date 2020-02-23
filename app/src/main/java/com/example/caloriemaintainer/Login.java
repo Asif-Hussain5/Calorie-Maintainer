0984f4f8202b20e4899cdcd7d53a4256d4618099
Asif <asifasfu@gmail.com>
2020-02-23 07:59:31 -0800

    Calorie maintainer with user records

--- app/src/main/java/com/example/caloriemaintainer/Login.java
+++ app/src/main/java/com/example/caloriemaintainer/Login.java
@@ -1,18 +1,26 @@
 package com.example.caloriemaintainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
-    EditText email,password;
+    EditText txt_email,txt_password;
     Button login,signup;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -21,47 +29,69 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+       txt_email = findViewById(R.id.email);
+        txt_password = findViewById(R.id.password);
         login = findViewById(R.id.login);
         signup = findViewById(R.id.signup);
+        firebaseAuth = FirebaseAuth.getInstance();
         //login.setOnClickListener(this);
 
-         login.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(),"You are logged in",Toast.LENGTH_SHORT).show();
-                //Intent i = new Intent(Login.this,Navbar.class);
-               // startActivity(i);
+                String email = txt_email.getText().toString().trim();
+                String password = txt_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+
+                    Toast.makeText(Login.this, "email is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+
+                    Toast.makeText(Login.this, "password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6) {
+
+                    Toast.makeText(Login.this, "password is too short", Toast.LENGTH_SHORT).show();
+                }
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                          startActivity(new Intent(getApplicationContext(),Navbar.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "login Failed", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
+
             }
         });
 
 
-        signup.setOnClickListener(new View.OnClickListener() {
+
+       signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(),"",Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(Login.this,signup.class);
                 startActivity(i);
 
-                if(!validate()){
-                    Toast.makeText(Login.this,"Success...",Toast.LENGTH_LONG).show();
 
-                }
             }
-            private boolean validate(){
-                if(email.getText().toString().trim().length()<=0){
 
-                    Toast.makeText(Login.this,"Enter your Email",Toast.LENGTH_SHORT).show();
-                    return true;
-                }else if (password.getText().toString().trim().length()<=0){
 
-                    Toast.makeText(Login.this,"Enter your Password",Toast.LENGTH_SHORT).show();
-                    return true;
-                }
 
-                return false;
-            }
+
         });
     }
 }
--- app/src/main/java/com/example/caloriemaintainer/signup.java
+++ app/src/main/java/com/example/caloriemaintainer/signup.java
@@ -1,5 +1,6 @@
 package com.example.caloriemaintainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,33 +12,105 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.QuickContactBadge;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
 
-    EditText name, email,pass,confirmpass;
+    EditText txt_name, txt_email,txt_pass,txt_confirmpass;
+    RadioGroup RadioGroup;
     RadioButton male,female;
     Button rgstr;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        name = findViewById(R.id.name);
-        email = findViewById(R.id.email);
-        pass = findViewById(R.id.Pass);
-        confirmpass = findViewById(R.id.confirmPass);
+        txt_name = findViewById(R.id.name);
+        txt_email = findViewById(R.id.email);
+        txt_pass = findViewById(R.id.Pass);
+        txt_confirmpass = findViewById(R.id.confirmPass);
+
         male = findViewById(R.id.male);
         female = findViewById(R.id.female);
         rgstr = findViewById(R.id.rgstr);
-
+            //firebase authentication//
+        firebaseAuth = FirebaseAuth.getInstance();
         rgstr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = txt_name.getText().toString().trim();
+                String email = txt_email.getText().toString().trim();
+                String password = txt_pass.getText().toString().trim();
+                String confrimpass = txt_confirmpass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(name)) {
+
+                    Toast.makeText(signup.this, "please enter your name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)) {
+
+                    Toast.makeText(signup.this, "email is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+
+                    Toast.makeText(signup.this, "password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(confrimpass)) {
+
+                    Toast.makeText(signup.this, "re_enter password ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+
+                    Toast.makeText(signup.this, "password is too short", Toast.LENGTH_SHORT).show();
+                }
+
+                if (password.equals(confrimpass)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        startActivity(new Intent(getApplicationContext(), Navbar.class));
+                                        Toast.makeText(signup.this, "You Are Registered", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(signup.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                    // ...
+                                }
+                            });
+
+
+
+
+        /*rgstr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Toast.makeText(getBaseContext(),"You Are Register",Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(signup.this, Login.class);
-                startActivity(i);
+                  Intent i = new Intent(signup.this, Login.class);
+                                      startActivity(i);
 
                 if(!validate()){
                     Toast.makeText(signup.this,"Success...",Toast.LENGTH_LONG).show();
@@ -65,9 +138,13 @@ public class signup extends AppCompatActivity {
 
                 return false;
             }
-        });
+        });*/
+
+            }
 
 
-
-    }
+        }
+            }
+        );}
 }
+
